b45e14743ee78aa5cfbc802851f493267a64ba22
Spencer Hall <spencer@hallblog.com>
2022-10-09 12:20:57 -0600

    Drink Model Created

--- app/FrappuccinoParadise/models.py
+++ app/FrappuccinoParadise/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 # Create your models here.
-class Inventory(models.Model):
+class Drink(models.Model):
     name = models.CharField(max_length=50)
     cost = models.IntegerField()
     canMake = models.BooleanField(default=False)
 
 
 class Ingredient(models.Model):
-    drinks = models.ManyToManyField(Inventory)
+    drinks = models.ManyToManyField(Drink)
