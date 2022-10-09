c428984b6b1badf74826b76258af5e66dabb7a8c
Spencer Hall <spencer@hallblog.com>
2022-10-09 12:45:26 -0600

    Drink and Ingredient Models complete

--- app/FrappuccinoParadise/models.py
+++ app/FrappuccinoParadise/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(default='', max_length=50)
+    cost = MoneyField(default=0, max_digits=5, decimal_places=2, default_currency='USD')
+    amountPurchased = models.IntegerField(default=0)
+
+
 class Drink(models.Model):
-    name = models.CharField(max_length=50)
-    cost = models.IntegerField()
+    name = models.CharField(default='', max_length=50)
+    cost = MoneyField(default=0, max_digits=5, decimal_places=2, default_currency='USD')
+    ingredients = models.ManyToManyField(Ingredient)
     canMake = models.BooleanField(default=False)
 
-
-class Ingredient(models.Model):
-    drinks = models.ManyToManyField(Drink)
