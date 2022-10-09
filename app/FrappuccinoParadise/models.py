18ef288b38d5a4335b49d04b1751abd0d0a31061
Spencer Hall <spencer@hallblog.com>
2022-10-08 22:48:13 -0600

    Inventory Model Created

--- app/FrappuccinoParadise/models.py
+++ app/FrappuccinoParadise/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Inventory(models.Model):
+    name = models.CharField(max_length=50)
+    cost = models.IntegerField()
+    canMake = models.BooleanField(default=False)
+
+
+class Ingredient(models.Model):
+    drinks = models.ManyToManyField(Inventory)
