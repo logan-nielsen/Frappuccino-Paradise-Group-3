975375f8bd0f61e10fb73439deebcf34cb886efc
Spencer Hall <spencer@hallblog.com>
2022-10-11 18:40:48 -0600

    Removed can_make field. Added support for djmoney

--- app/FrappuccinoParadise/models.py
+++ app/FrappuccinoParadise/models.py
@@ -13,7 +13,6 @@ class Drink(models.Model):
     name = models.CharField(default='', max_length=50)
     cost = MoneyField(default=0, max_digits=5, decimal_places=2, default_currency='USD')
     ingredients = models.ManyToManyField(Ingredient)
-    canMake = models.BooleanField(default=False)
 
 
 class Order(models.Model):
