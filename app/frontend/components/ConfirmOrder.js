7a7d8b11bf6b5723089993aee7e42f874cf74be2
Logan Nielsen <logan.nielsen@usu.edu>
2022-10-20 20:47:40 -0600

    added interface for adding a drink

--- app/frontend/components/ConfirmOrder.js
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState } from 'react';
-
-export default function ConfirmOrder() {
-    return (
-        <p>Confirm Order</p>
-    );
-}
--- app/frontend/pages/OrderPage.js
+++ app/frontend/pages/OrderPage.js
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Button from '@mui/material/Button';
-import SelectAddOns from '../components/SelectAddOns';
-import ConfirmOrder from '../components/ConfirmOrder';
-import SelectDrink from '../components/SelectDrink';
+import DrinkDialog from '../components/DrinkDialog';
+import { Button } from '@mui/material';
 
 const steps = ['Select Drink', 'Add Ons', 'Confirm Order'];
 const theme = createTheme();
 
 export default function OrderPage() {
-  const [activeStep, setActiveStep] = useState(0);
+  const [drinkDialogOpen, setDrinkDialogOpen] = useState(false)
+  const [drinks, setDrinks] = useState([])
+
+  useEffect(() => {
+    console.log(drinks)
+  }, [drinks])
 
   function handleNext() {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -30,73 +28,25 @@ export default function OrderPage() {
     console.log("order placed");
   }
 
+  function addDrink(drink) {
+    setDrinks([...drinks, drink])
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Container component="main">
         <CssBaseline />
-        <Box sx={{ width: '100%' }}>
-          <Stepper activeStep={activeStep}>
-            {steps.map((label, index) => {
-              const stepProps = {};
-              const labelProps = {};
-              return (
-                <Step key={label} {...stepProps}>
-                  <StepLabel {...labelProps}>{label}</StepLabel>
-                </Step>
-              );
-            })}
-          </Stepper>
-
-          {/* Conditionally render based on which step the user is on */}
-          {activeStep === 0 && (
-            <>
-            <SelectDrink />
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Button
-                color="inherit"
-                disabled
-                sx={{ mr: 1 }}
-              >
-                Back
-              </Button>
-              <Box sx={{ flex: '1 1 auto' }} />
-
-              <Button onClick={handleNext}>Next</Button>
-            </Box>
-            </>
-          )}
-          {activeStep === 1 && (
-            <>
-            <SelectAddOns />
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Button
-                onClick={handleBack}
-                sx={{ mr: 1 }}
-              >
-                Back
-              </Button>
-              <Box sx={{ flex: '1 1 auto' }} />
-  
-              <Button onClick={handleNext}>Next'</Button>
-            </Box>
-            </>
-          )}
-          {(activeStep == 2 &&
-            <>
-            <ConfirmOrder />
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Button
-                onClick={handleBack}
-                sx={{ mr: 1 }}
-              >
-                Back
-              </Button>
-              <Box sx={{ flex: '1 1 auto' }} />
-              <Button onClick={placeOrder}>Place Order</Button>
-            </Box>
-            </>
-          )}
-        </Box>
+        <Button 
+          variant="contained"
+          onClick={() => setDrinkDialogOpen(true)}
+        >
+          Add Drink
+        </Button>
+        <DrinkDialog 
+          addDrink={addDrink}
+          open={drinkDialogOpen} 
+          setOpen={setDrinkDialogOpen} 
+        />
       </Container>
     </ThemeProvider>
   );
