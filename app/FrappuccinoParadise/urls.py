0881d49366bcade497a73bfb6b5f68084b5e8704
gtonks <a02288558@aggies.usu.edu>
2022-10-21 12:11:53 -0600

    Saving changes before pulling

--- app/FrappuccinoParadise/urls.py
+++ app/FrappuccinoParadise/urls.py
@@ -1,10 +1,11 @@
 from django import views
-from django.urls import re_path
+from django.urls import re_path, path
 from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
   # other patterns here
   re_path('api/test', views.api, name='api'),
-  re_path(r'.*', TemplateView.as_view(template_name='FrappuccinoParadise/index.html'))
+  re_path(r'.*', TemplateView.as_view(template_name='FrappuccinoParadise/index.html')),
+  path("loghours/", views.add_shift, "add_shift"),
 ]
--- app/FrappuccinoParadise/views.py
+++ app/FrappuccinoParadise/views.py
@@ -1,4 +1,24 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+def is_employee(user):
+    return user.groups.filter(name="Baristas").exists()
 
 def api(request):
     return JsonResponse({'test': True})
+
+def hourLog(request):
+    pass
+
+# For barista to log a shift
+@login_required
+@user_passes_test(is_employee)
+def add_shift(request):
+    date = request.POST['date']
+    hours = request.POST['hours']
+    try:
+        request.user.account.timecard_set.create(date=date, hours=hours)
+    except:
+    # TODO: Render shift log here
+        return HttpResponse("Error logging shift")
+    return HttpResponse("Shift log successful!")
