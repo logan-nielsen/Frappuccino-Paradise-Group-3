dbbc7294b3ddaac8e262d8d557eb902fd8ff29f8
Logan Nielsen <logan.nielsen@usu.edu>
2022-11-07 17:24:40 -0700

    finished buy inventory page

--- app/FrappuccinoParadise/urls.py
+++ app/FrappuccinoParadise/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
   path("api/getrecipe/", views.get_recipe, name="get_recipe"),
   path("api/setorderready/", views.set_order_ready, name="set_order_ready"),
   path("api/setorderdelivered/", views.set_order_delivered, name="set_order_delivered"),
+  path("api/buyingredients/", views.buy_ingredients, name="buy_ingredients"),
   re_path(r'^(?!api.*$).*', views.index, name='index'),
 ]
