55cd80a37578ab67f59a976bf2e569793c97849b
Justin Roylance <juppenstein02@gmail.com>
2022-11-16 20:58:44 -0700

    finished addmenuitem unit test

--- app/FrappuccinoParadise/tests/testEditMenu.py
+++ app/FrappuccinoParadise/tests/testEditMenu.py
@@ -6,15 +6,14 @@ from FrappuccinoParadise.models import Drink, Order, Ingredient, OrderItem, Acco
 
 # Create your tests here.
 class EditMenuTest(TestCase):
-
-    def setup(self):
+    def setUp(self):
         self.user = get_user_model().objects.get(username='manager')
         self.client.force_login(self.user)
 
     def testAddMenuItem(self):
         request =  {'name': 'Rollaccino',
-                    'price': '8',
-                    'ingredients': [{"id":1,"name":"Coffee Beans","cost_currency":"USD","cost":"1.00","amountPurchased":86,"number":"1"},
+                    'price': '10',
+                    'ingredients': '''[{"id":1,"name":"Coffee Beans","cost_currency":"USD","cost":"1.00","amountPurchased":86,"number":"1"},
                                     {"id":2,"name":"Milk","cost_currency":"USD","cost":"1.00","amountPurchased":93,"number":"1"},
                                     {"id":3,"name":"Cinnamon","cost_currency":"USD","cost":"0.50","amountPurchased":93,"number":"1"},
                                     {"id":4,"name":"Ice Cream","cost_currency":"USD","cost":"2.00","amountPurchased":93,"number":"1"},
@@ -23,10 +22,8 @@ class EditMenuTest(TestCase):
                                     {"id":7,"name":"Creamer","cost_currency":"USD","cost":"0.50","amountPurchased":120,"number":"1"},
                                     {"id":8,"name":"Matcha","cost_currency":"USD","cost":"1.00","amountPurchased":94,"number":"1"},
                                     {"id":9,"name":"Vanilla","cost_currency":"USD","cost":"1.00","amountPurchased":93,"number":"1"},
-                                    {"id":10,"name":"Honey","cost_currency":"USD","cost":"1.00","amountPurchased":93,"number":"1"}]}
-        response = self.client.post('/app/api/addmenuitem/', request)
-        print(response.content)
-        print(response.status)
+                                    {"id":10,"name":"Honey","cost_currency":"USD","cost":"1.00","amountPurchased":93,"number":"1"}]'''}
+        response = self.client.post('/app/api/addmenuitem/', request)    
         self.assertJSONEqual(str(response.content, encoding='utf8'), {"error": None})
         drink = Drink.objects.get(name='Rollaccino')
         self.assertEqual(drink.name, 'Rollaccino')
@@ -34,13 +31,14 @@ class EditMenuTest(TestCase):
         ingredientids = []
         for i in drink.ingredientitem_set.all().values():
             ingredientids.append(i['ingredient_id'])
-        self.assertContains(ingredientids, text='1', count=1)
-        self.assertContains(ingredientids, text='2', count=1)
-        self.assertContains(ingredientids, text='3', count=1)
-        self.assertContains(ingredientids, text='4', count=1)
-        self.assertContains(ingredientids, text='5', count=1)
-        self.assertContains(ingredientids, text='6', count=1)
-        self.assertContains(ingredientids, text='7', count=1)
-        self.assertContains(ingredientids, text='8', count=1)
-        self.assertContains(ingredientids, text='9', count=1)
-        self.assertContains(ingredientids, text='10', count=1)
+            self.assertTrue(i['number'] == 1)
+        self.assertTrue(1 in ingredientids)
+        self.assertTrue(2 in ingredientids)
+        self.assertTrue(3 in ingredientids)
+        self.assertTrue(4 in ingredientids)
+        self.assertTrue(5 in ingredientids)
+        self.assertTrue(6 in ingredientids)
+        self.assertTrue(7 in ingredientids)
+        self.assertTrue(8 in ingredientids)
+        self.assertTrue(9 in ingredientids)
+        self.assertTrue(10 in ingredientids)
